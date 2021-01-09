e58278a45361c1818259a585346fbb49ac89d4ae
Cirrus Mokhtari <Cirrus1994@gmail.com>
2021-01-09 10:23:35 -0800

    beautiful soup parses elements and finds links

--- main.py
+++ main.py
@@ -5,5 +5,16 @@ search = input("Enter Search Term: ")
 params = {'q': search}
 r = requests.get('http://www.bing.com/search', params=params)
 
-soup = BeautifulSoup(r.text)
-print(soup.prettify())
+soup = BeautifulSoup(r.text, 'html.parser')
+#searching for ol element, with id attribute of b_results
+results = soup.find(f"ol", {'id' : "b_results"})
+#searching for li element with b_algo class
+links = results.findAll('li', {'class': 'b_algo'})
+
+for item in links:
+    item_text = item.find('a').text
+    item_href = item.find('a').attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
